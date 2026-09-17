716ac5551b6828ca90c82b4bd763fa349f8f7441
LE MINH QUANG <leminhquang.td2004@gmail.com>
2026-09-17 15:16:40 +0700

    Initalize commit

---
+++ backend/src/main/java/com/app/Q_Entertainment/QEntertainmentApplication.java
@@ -0,0 +1,13 @@
+package com.app.Q_Entertainment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QEntertainmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QEntertainmentApplication.class, args);
+	}
+
+}
