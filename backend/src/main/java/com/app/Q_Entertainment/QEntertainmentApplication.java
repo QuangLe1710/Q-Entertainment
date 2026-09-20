11165008ca2ce8651e1e8aee8e9412b665dbec84
QuangLe1710 <leminhquang.td2004@gmail.com>
2026-09-21 01:15:00 +0700

    config Time Zone

--- backend/src/main/java/com/app/Q_Entertainment/QEntertainmentApplication.java
+++ backend/src/main/java/com/app/Q_Entertainment/QEntertainmentApplication.java
@@ -3,10 +3,13 @@ package com.app.Q_Entertainment;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class QEntertainmentApplication {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Ho_Chi_Minh"));
 		SpringApplication.run(QEntertainmentApplication.class, args);
 	}
 
